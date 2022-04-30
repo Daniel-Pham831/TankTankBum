3aa89873b9af228312c07c830dbf3e5cf3384d50
Phúc Phạm <phamphuc0603@gmail.com>
2022-04-30 11:36:26 +0700

    Added name above tanks added indicator for tanks added camera follow tank

---
+++ Assets/Scripts/Tank/TankCamera.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankCamera : MonoBehaviour
+{
+    [SerializeField] private Vector3 attackerPositionView;
+    [SerializeField] private Vector3 attackerRotationEulerView;
+    [SerializeField] private Vector3 defenderPositionView;
+    [SerializeField] private Vector3 defenderRotationEulerView;
+
+    public Camera ActualTankCamera;
+    public Quaternion GetActualCameraRot => ActualTankCamera.transform.rotation;
+
+    private GameObject tank;
+
+    private void Update()
+    {
+        if (tank != null)
+        {
+            transform.position = tank.transform.position;
+        }
+    }
+
+    public void SetupTankCamera(GameObject tankToFollow, Role tankRole)
+    {
+        tank = tankToFollow;
+        SetCameraBasedOnRole(tankRole);
+    }
+
+    public void SetCameraBasedOnRole(Role role)
+    {
+        Vector3 targetPositionView = role == Role.Attacker ? attackerPositionView : defenderPositionView;
+        Vector3 targetRotationEulerView = role == Role.Attacker ? attackerRotationEulerView : defenderRotationEulerView;
+
+        ActualTankCamera.transform.position = targetPositionView;
+        ActualTankCamera.transform.rotation = Quaternion.Euler(targetRotationEulerView);
+    }
+}
--- Assets/Scripts/Tank/TankManager.cs
+++ Assets/Scripts/Tank/TankManager.cs
@@ -12,19 +12,28 @@ public class TankManager : MonoBehaviour
 {
     public static TankManager Singleton { get; private set; }
     [SerializeField] private GameObject tankPrefab;
+    [SerializeField] private GameObject tankNamePrefab;
+    [SerializeField] private GameObject localTankCameraPrefab;
     [SerializeField] private Material blueTankMaterial;
     [SerializeField] private Material redTankMaterial;
     [SerializeField] private GameObject[] spawnPositions;
 
+    // Tanks data
+    public TankCamera LocalTankCamera { get; set; }
+    public TankInformation LocalTankInformation { get; set; }
+    public Dictionary<byte, TankInformation> TankInformations { get; set; }
     public Dictionary<byte, GameObject> TankObjects { get; set; }
     public Dictionary<byte, Rigidbody> TankRigidbodies { get; set; }
+    public Dictionary<byte, TankName> TankNames { get; set; }
 
 
     private void Awake()
     {
         Singleton = this;
+        TankInformations = new Dictionary<byte, TankInformation>();
         TankObjects = new Dictionary<byte, GameObject>();
         TankRigidbodies = new Dictionary<byte, Rigidbody>();
+        TankNames = new Dictionary<byte, TankName>();
 
         DontDestroyOnLoad(gameObject);
     }
@@ -55,34 +64,64 @@ public class TankManager : MonoBehaviour
         Player myPlayer = clientInformation.MyPlayerInformation;
         List<Player> otherPlayers = clientInformation.PlayerList;
 
-        SpawnTank(myPlayer.Id, myPlayer.Team, true, clientInformation.IsHost);
+        SpawnTank(myPlayer.Id, myPlayer.Team, myPlayer.Name, true, clientInformation.IsHost);
 
         foreach (Player player in otherPlayers)
         {
-            SpawnTank(player.Id, player.Team, false, clientInformation.IsHost);
+            SpawnTank(player.Id, player.Team, player.Name, false, clientInformation.IsHost);
         }
 
         if (clientInformation.IsHost)
         {
             TankServerManager.Singleton.TankRigidbodies = TankRigidbodies;
         }
+
+        SetupAllNamesData();
     }
 
-    private void SpawnTank(byte id, Team team, bool isOwner, bool isHost)
+
+
+    private void SpawnTank(byte id, Team team, string name, bool isLocalPlayer, bool isHost)
     {
         GameObject tank = Instantiate(tankPrefab, spawnPositions[id].transform.position, Quaternion.identity);
-        SetTankColorBasedOnTeam(tank, team);
         Rigidbody tankRigid = tank.GetComponent<Rigidbody>();
-        TankInformation tNetwork = tank.GetComponent<TankInformation>();
-        tNetwork.ID = id;
-        tNetwork.Team = team;
-        tNetwork.IsLocalPlayer = isOwner;
-        tNetwork.IsHost = isHost;
-        Debug.Log($"Tank spawned with ID:{tNetwork.ID}");
+        TankInformation tankInformation = tank.GetComponent<TankInformation>();
+        tankInformation.ID = id;
+        tankInformation.Team = team;
+        tankInformation.IsLocalPlayer = isLocalPlayer;
+        tankInformation.IsHost = isHost;
 
+        if (isLocalPlayer)
+        {
+            LocalTankInformation = tankInformation;
+            SetLocalTankCamera(tank, team);
+        }
 
-        TankObjects.Add(tNetwork.ID, tank);
-        TankRigidbodies.Add(tNetwork.ID, tankRigid);
+        SetTankName(id, tank, name);
+        SetTankColorBasedOnTeam(tank, team);
+
+        TankInformations.Add(tankInformation.ID, tankInformation);
+        TankObjects.Add(tankInformation.ID, tank);
+        TankRigidbodies.Add(tankInformation.ID, tankRigid);
+    }
+
+    private void SetLocalTankCamera(GameObject tank, Team team)
+    {
+        GameObject localTankCameraObject = Instantiate(localTankCameraPrefab);
+        TankCamera localTankCameraScript = localTankCameraObject.GetComponent<TankCamera>();
+
+        localTankCameraScript.SetupTankCamera(tank, team == Team.Blue ? Role.Defender : Role.Attacker);
+
+        LocalTankCamera = localTankCameraScript;
+    }
+
+    private void SetTankName(byte id, GameObject tank, string name)
+    {
+        GameObject tankNameObject = Instantiate(tankNamePrefab);
+        TankName tankNameScript = tankNameObject.GetComponent<TankName>();
+        tankNameScript.SetUpTankName(tank, name);
+
+        TankNames.Add(id, tankNameScript);
     }
 
     private void SetTankColorBasedOnTeam(GameObject tank, Team team)
@@ -91,6 +130,15 @@ public class TankManager : MonoBehaviour
         foreach (MeshRenderer meshRenderer in meshRenderers)
         {
             meshRenderer.material = team == Team.Blue ? blueTankMaterial : redTankMaterial;
+        }
+    }
+
+    private void SetupAllNamesData()
+    {
+        foreach (byte id in TankNames.Keys)
+        {
+            TankNames[id].SetNameRot(LocalTankCamera.GetActualCameraRot);
+            TankNames[id].SetNameColor(LocalTankInformation.Team, TankInformations[id].Team);
         }
     }
     #endregion
